073ec93a2046d94dd27803fee800b9d122758c36
Heartbroken-Git <corentin196@gmail.com>
2018-07-30 23:54:13 +0200

    first APDUs in, based on ACR38X technical manual

--- BRutils.py
+++ BRutils.py
@@ -26,3 +26,16 @@ class const:
 	# Statuses for the prompt
 	STATUS_PROMPT_DISCONNECTED = 10
 	STATUS_PROMPT_EXITING = 20
+
+## @brief Class containing APDUs to transmit
+#  @sa https://hashtips.files.wordpress.com/2013/10/pma-acr38xccid-6-02.pdf
+class apdu:
+	SELECT_CARD_TYPE = [0xFF,0xA4,0x00,0x00,0x01,0x06]
+	READ_MEMORY_CARD = [0xFF,0xB0,0x00] # + byte address of first byte to read + length of data to read (in bytes ?)
+	READ_PRESENTATION_ERROR_COUNTER_MEMORY_CARD = [0xFF,0xB1,0x00,0x00,0x04]
+	READ_PROTECTION_BITS = [0xFF,0xB2,0x00,0x00,0x04]
+	WRITE_MEMORY_CARD = [0xFF,0xD0,0x00] # + byte address of first byte to write + length of data to write (in bytes ?) + every byte to write
+	WRITE_PROTECTION_MEMORY_CARD = [0xFF,0xD1,0x00] # + byte address of first byte to write (from 0x00 to 0x1F) + length of data to write (in bytes ?) + every byte to write
+	PRESENT_CODE_MEMORY_CARD = [0xFF,0x20,0x00,0x00,0x03] + # + each of the three bytes of the PIN
+	CHANGE_CODE_MEMORY_CARD = [0xFF,0xD2,0x00,0x01,0x03] # + each of the three bytes of the new PIN
+	
